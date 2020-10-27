4e722b99fe0d3783241358991b2f8e81fb1d1fc6
Onno <onno@backbase.com>
2020-10-27 08:46:31 +0100

    add protractor flake

---
+++ config/protractor.flake.js
@@ -0,0 +1,14 @@
+const protractorFlake = require('protractor-flake');
+// skip first two passed args (node and self)
+let protractorArgs = process.argv.splice(2);
+
+protractorFlake({
+  protractorPath: 'node_modules/protractor/bin/protractor',
+  maxAttempts: 3,
+  parser: 'standard',
+  nodeBin: 'node',
+  protractorArgs: protractorArgs,
+  displayStacktrace: 'all'
+}, (status) => {
+  process.exit(status);
+});
