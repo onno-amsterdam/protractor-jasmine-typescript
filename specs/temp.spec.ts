e6f9a7d7404d38929cddc2cfaf65379b84c3e11b
Onno <onno@backbase.com>
2020-10-27 10:34:36 +0100

    fix retry

---
+++ specs/temp.spec.ts
@@ -0,0 +1,13 @@
+describe('TEST', async () => {
+    it('pass', async () => {
+        expect(true).toBe(true);
+    });
+
+    it('fail', async () => {
+        expect(true).toBe(false);
+    });
+
+    it('pass', async () => {
+        expect(true).toBe(true);
+    });
+})
